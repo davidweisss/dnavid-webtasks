6a9592944f22f9d3f99e08fa163083eebb7dfcaf
David Weiss <davidweiss@dnavid.com>
2017-01-23 13:59:23 +0100

    Update createUser.js

--- createUser.js
+++ createUser.js
@@ -1,10 +1,49 @@
+////////////////////////////////////////////////
+// Libraries
+////////////////////////////////////////////////
 'use latest';
 
-import express from 'express';
-import { fromExpress } from 'webtask-tools';
+// DB access
 import bodyParser from 'body-parser';
+import express from 'express';
+import Webtask from 'webtask-tools';
+import { MongoClient } from 'mongodb';
+import { ObjectID } from 'mongodb';
+
+// Authentication
+// import express from 'express';
+import { fromExpress } from 'webtask-tools';
+// import bodyParser from 'body-parser';
 const app = express();
 
+////////////////////////////////////////////////
+// Body
+////////////////////////////////////////////////
+
+// DB access 
+const collection = 'users';
+const server = express();
+
+
+server.use(bodyParser.json());
+
+
+server.get('/', (req, res, next) => {
+  const { MONGO_URL, pseudo } = req.webtaskContext.data;
+  MongoClient.connect(MONGO_URL, (err, db) => {
+    db.collection(collection).findOne({_id: pseudo}, (err, result) => {
+        if (err) return next(err);
+      db.close();
+      if (err) return next(err);
+      res.status(200).send(result);
+    });
+  });
+});
+module.exports = Webtask.fromExpress(server);
+
+
+
+// Authentication
 app.use(bodyParser.json());
 
 // To retrieve JSON Web Key
