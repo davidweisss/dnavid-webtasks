31aace2fb8381cbd37a94e1ef4e840b8202a1c32
David Weiss <davidweiss@dnavid.com>
2017-01-23 13:23:25 +0100

    Update authenticateUser.js

--- authenticateUser.js
+++ authenticateUser.js
@@ -1,1 +1,33 @@
+'use latest';
 
+import express from 'express';
+import { fromExpress } from 'webtask-tools';
+import bodyParser from 'body-parser';
+const app = express();
+
+app.use(bodyParser.json());
+
+// To retrieve JSON Web Key
+const jwksRsa = require('jwks-rsa');
+const jwt = require('express-jwt');
+
+app.use((req, res, next) => { 
+  const issuer = 'https://' + req.webtaskContext.secrets.AUTH0_DOMAIN + '/';
+  jwt({
+    secret: new Buffer(req.webtaskContext.secrets.CLIENT_SECRET, 'base64'),
+    audience: req.webtaskContext.secrets.AUDIENCE,
+    issuer: issuer,
+  })(req, res, next);
+});
+
+app.get('/test', (req, res) => {
+  // test endpoint, no-operation
+  res.send(200);
+});
+
+app.get('/', (req, res) => {
+  // add your logic, you can use scopes from req.user
+  res.json({hi : req.user.sub});
+});
+
+module.exports = fromExpress(app);
