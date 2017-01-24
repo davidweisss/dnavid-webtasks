405060813dc88357e7d014f61db7c0734be9c29e
David Weiss <davidweiss@dnavid.com>
2017-01-24 14:33:15 +0100

    Update doesUserHaveProfile.js

--- doesUserHaveProfile.js
+++ doesUserHaveProfile.js
@@ -44,6 +44,7 @@ app.get('/', (req, res, next) => {
       },
         (err, result) => {
       db.close();
+      console.log(result)
       if (err) return next(err);
       res.status(201).send(result);
     });
